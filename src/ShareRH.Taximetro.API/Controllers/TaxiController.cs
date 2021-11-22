c9ad8d51e2e6e639c3ea7f1bb4f55df51b8d056d
cristiano.fietto <cristiano.fietto@capgemini.com>
2021-11-22 18:20:05 -0300

    Add project files.

---
+++ src/ShareRH.Taximetro.API/Controllers/TaxiController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareRH.Taximetro.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxiCalculationController : ControllerBase
+    {
+        public List<string> passag = new List<string>();
+
+        [HttpPost] public double Calctrip(DateTime hours,int dist,string nomePassString,string param2)
+        {
+            if (hours != null)
+            {
+                if (dist > 0 && dist != 0)
+                {
+                    double valuePerMt = 0.1;
+                    //Mudança de município
+                    if (dist > 100)
+                    {
+                        valuePerMt = 0.15; }
+                    //Valor começa com zero
+                    double result = 0;
+                    //Bandeira 2
+                    if (hours.Hour > 22 || hours.Hour < 6) {
+                                valuePerMt *= 2; }
+                    //é domingo
+                    else if (hours.DayOfWeek == 0)
+                    {
+                    valuePerMt *= 2;
+                    }
+
+                    passag.Add(nomePassString);
+                    //Distancia * o valor 
+                    result = valuePerMt * dist; return result;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+
+        [HttpGet]
+        public string NomeUltimo() { var ult = ""; if(passag.Count > 3) { ult = passag.LastOrDefault(); } else { ult = "vc não completou a quantidade de passageiros suficientes"; } return ult; }
+    }
+}
